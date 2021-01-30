ad4d31339f466cf27d854a595fc3c9c579630552
Shahzod Akhmedov <77686662+Shahzodjohn@users.noreply.github.com>
2021-01-30 12:15:37 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp80/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ConsoleApp76
+{
+
+
+
+    class rectangle
+    {
+        public double side1 { get; set; }
+        public double side2 { get; set; }
+
+
+        public rectangle(double side1, double side2)
+        {
+            this.side1 = side1;
+            this.side2 = side2;
+        }
+        public double AreaCalcilator()
+        {
+            return side1 * side2;
+        }
+        public double perimetercalculator()
+        {
+            return 2 * (side1 + side2);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("a = ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("b = ");
+            double b = double.Parse(Console.ReadLine());
+            rectangle rectangle = new rectangle(a, b);
+            double area = rectangle.AreaCalcilator();
+            double perimeter = rectangle.perimetercalculator();
+            Console.WriteLine(area);
+            Console.WriteLine(perimeter);
+            Console.ReadKey();
+
+        }
+    }
+}   
